0fe67e2ecffa28d0899698150c37d6a3b60ffb24
Hiroaki Nagashima <hi6aki_7ga40@hotmail.com>
2019-06-25 00:04:31 +0900

    センサー値表示を追加

--- app_iothub_client/pinkit/pinkit.c
+++ app_iothub_client/pinkit/pinkit.c
@@ -4,11 +4,21 @@
 
 #include "pinkit.h"
 #include <math.h>
+#include "gpio_api.h"
+#include "syssvc/syslog.h"
 
 pinkit_t pinkit;
 
+gpio_t user_led;
+
 void pinkit_task(intptr_t exinf)
 {
+#if SENSOR_DEBUG
+	int iterator = 0;
+#endif
+	gpio_init(&user_led, LED_USER);
+	gpio_mode(&user_led, PIN_OUTPUT);
+
 	Accelerometer_Init(100, 1000);
 	Temperature_Init();
 	BoardFullColorLED_Init();
@@ -16,10 +26,20 @@ void pinkit_task(intptr_t exinf)
 
 	for (;;) {
 		Accelerometer_TakeMeasurements(&pinkit.accel);
-		BoardFullColorLED_SetRgb(fabs(pinkit.accel.X) > 0.5, fabs(pinkit.accel.Y) > 0.5, fabs(pinkit.accel.Z) > 0.5);
 		pinkit.temperature = Temperature_TakeMeasurement();
 		pinkit.humidity = LightSensor_TakeMeasurement();
 
+		BoardFullColorLED_SetRgb(fabs(pinkit.accel.X) > 0.5, fabs(pinkit.accel.Y) > 0.5, fabs(pinkit.accel.Z) > 0.5);
+		gpio_write(&user_led, pinkit.temperature > 28.0);
+#if SENSOR_DEBUG
+		iterator++;
+		if (iterator >= 100){
+			char text[80];
+			iterator = 0;
+			sprintf(text, "x:%0.3f, y:%0.3f, z:%0.3f, temp:%0.1f", pinkit.accel.X, pinkit.accel.Y, pinkit.accel.Z, pinkit.temperature);
+			syslog(LOG_NOTICE, text);
+		}
+#endif
 		dly_tsk(1000);
 	}
 }
